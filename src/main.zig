4d4a091647e32a9e8a511f4625e00064b5cd0eab
Niklas Koll <niklas@nkoll.de>
2024-11-17 15:25:04 +0100

    Update Grammar

--- src/main.zig
+++ src/main.zig
@@ -3,3 +3,12 @@ const std = @import("std");
 pub fn main() !void {
     std.debug.print("Hello World\n", .{});
 }
+
+test "for" {
+    const str1: []const u8 = "Hallo";
+    const str2: []const u8 = "Welt!";
+
+    for (str1, str2) |char1, char2| {
+        std.debug.print("Left: {c}, Right: {c}\n", .{ char1, char2 });
+    }
+}
