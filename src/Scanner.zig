73b0bfc6fd3c254e816094083796948c2e81e208
Niklas Koll <niklas@nkoll.de>
2024-11-17 21:07:58 +0100

    Finished Scanner i think, pls write more tests, thank

--- src/Scanner.zig
+++ src/Scanner.zig
@@ -4,7 +4,8 @@ tokens: std.ArrayList(Token),
 start: usize = 0,
 current: usize = 0,
 line: usize = 1,
-column: usize = 0,
+column: usize = 1,
+lexeme_column: usize = 0,
 has_error: bool = false,
 
 pub fn scan(alloc: Allocator, input: []const u8) ![]const Token {
@@ -20,16 +21,16 @@ pub fn scan(alloc: Allocator, input: []const u8) ![]const Token {
 fn scanTokens(self: *Scanner) !void {
     while (!self.isAtEnd()) {
         self.start = self.current;
+        self.lexeme_column = self.column;
         try self.nextToken();
     }
-    self.column += 1;
 
     try self.tokens.append(.{
         .type = .EOF,
         .lexeme = "",
         .location = .{
             .line = self.line,
-            .column = self.column,
+            .column = self.lexeme_column,
         },
     });
 }
@@ -37,9 +38,14 @@ fn scanTokens(self: *Scanner) !void {
 fn nextToken(self: *Scanner) !void {
     const c = self.advance();
     switch (c) {
+        ' ', '\t', '\r' => {},
+        '\n' => {
+            self.line += 1;
+            self.column = 1;
+            self.lexeme_column = 1;
+        },
         '+' => try self.makeToken(.@"+"),
         '*' => try self.makeToken(.@"*"),
-        '/' => try self.makeToken(.@"/"),
         ':' => try self.makeToken(.@":"),
         ';' => try self.makeToken(.@";"),
         '(' => try self.makeToken(.@"("),
@@ -50,6 +56,15 @@ fn nextToken(self: *Scanner) !void {
         '=' => try self.makeToken(if (self.match('=')) .@"==" else .@"="),
         '!' => try self.makeToken(if (self.match('=')) .@"!=" else .@"="),
         '>' => try self.makeToken(if (self.match('=')) .@">=" else .@">"),
+        '"' => try self.string(),
+        '0'...'9' => try self.number(),
+        '/' => {
+            if (self.match('/')) {
+                while (!self.check('\n')) _ = self.advance();
+            } else {
+                try self.makeToken(.@"/");
+            }
+        },
         '<' => {
             if (self.match('-')) {
                 try self.makeToken(.@"<-");
@@ -59,10 +74,53 @@ fn nextToken(self: *Scanner) !void {
                 try self.makeToken(.@"<");
             }
         },
-        else => {
-            // TODO: Keywords and Literals
-        },
+        else => try self.keywordOrIdentifier(),
     }
+}
+
+fn string(self: *Scanner) !void {
+    while (!self.check('"') and !self.isAtEnd()) {
+        if (self.check('\n')) {
+            self.line += 1;
+            self.column = 0;
+        }
+        _ = self.advance();
+    }
+
+    if (self.isAtEnd()) {
+        self.has_error = true;
+        std.debug.print("Syntax Error: unterminated string\n", .{});
+    }
+
+    _ = self.advance(); // closing "
+
+    const value = self.input[self.start + 1 .. self.current - 1];
+    try self.makeTokenWithValue(.string_literal, value);
+}
+
+fn number(self: *Scanner) !void {
+    while (std.ascii.isDigit(self.peek())) {
+        _ = self.advance();
+    }
+
+    if (self.check('.') and std.ascii.isDigit(self.peekNext())) {
+        _ = self.advance();
+        while (std.ascii.isDigit(self.peek())) {
+            _ = self.advance();
+        }
+    }
+
+    try self.makeToken(.number);
+}
+
+fn keywordOrIdentifier(self: *Scanner) !void {
+    while (std.ascii.isAlphanumeric(self.peek())) {
+        _ = self.advance();
+    }
+
+    const text = self.input[self.start..self.current];
+    const token_type: Token.Type = Token.ReservedKeywords.get(text) orelse .identifier;
+    try self.makeToken(token_type);
 }
 
 fn advance(self: *Scanner) u8 {
@@ -79,6 +137,21 @@ fn match(self: *Scanner, expected: u8) bool {
     return true;
 }
 
+fn peek(self: *Scanner) u8 {
+    if (self.isAtEnd()) return 0;
+    return self.input[self.current];
+}
+
+fn peekNext(self: *Scanner) u8 {
+    if (self.current + 1 >= self.input.len) return 0;
+    return self.input[self.current + 1];
+}
+
+fn check(self: *Scanner, expected: u8) bool {
+    if (self.isAtEnd()) return false;
+    return self.input[self.current] == expected;
+}
+
 fn isAtEnd(self: *Scanner) bool {
     return self.current >= self.input.len;
 }
@@ -90,7 +163,18 @@ fn makeToken(self: *Scanner, token_type: Token.Type) !void {
         .lexeme = lexeme,
         .location = .{
             .line = self.line,
-            .column = self.column,
+            .column = self.lexeme_column,
+        },
+    });
+}
+
+fn makeTokenWithValue(self: *Scanner, token_type: Token.Type, value: []const u8) !void {
+    try self.tokens.append(.{
+        .type = token_type,
+        .lexeme = value,
+        .location = .{
+            .line = self.line,
+            .column = self.lexeme_column,
         },
     });
 }
@@ -100,28 +184,50 @@ const Scanner = @This();
 const std = @import("std");
 const Token = @import("Token.zig");
 const Allocator = std.mem.Allocator;
+const testing = std.testing;
+
+fn expectTokensEqual(actual: []const Token, expected: []const Token) !void {
+    try testing.expect(actual.len == expected.len);
+    if (actual.len == expected.len) {
+        for (expected, actual) |e, a| {
+            try testing.expectEqual(e.type, a.type);
+            try testing.expectEqual(e.location, a.location);
+            try testing.expectEqualSlices(u8, e.lexeme, a.lexeme);
+        }
+    }
+}
 
 test scan {
-    const testing = std.testing;
     const alloc = testing.allocator;
 
     const input =
-        \\var name: string = "FlowLang";
+        \\var name: string = "FlowLang"; // This will be ignored
+        \\const num: float = 12.34;
+        \\
     ;
 
     const tokens = try scan(alloc, input);
     defer alloc.free(tokens);
 
-    const expected_tokens: []const Token = &.{
+    const expected: []const Token = &.{
         .{ .type = .@"var", .lexeme = "var", .location = .{ .line = 1, .column = 1 } },
         .{ .type = .identifier, .lexeme = "name", .location = .{ .line = 1, .column = 5 } },
         .{ .type = .@":", .lexeme = ":", .location = .{ .line = 1, .column = 9 } },
         .{ .type = .string, .lexeme = "string", .location = .{ .line = 1, .column = 11 } },
         .{ .type = .@"=", .lexeme = "=", .location = .{ .line = 1, .column = 18 } },
-        .{ .type = .string_literal, .lexeme = "FlowLang", .location = .{ .line = 1, .column = 21 } },
+        .{ .type = .string_literal, .lexeme = "FlowLang", .location = .{ .line = 1, .column = 20 } },
         .{ .type = .@";", .lexeme = ";", .location = .{ .line = 1, .column = 30 } },
-        .{ .type = .EOF, .lexeme = "", .location = .{ .line = 1, .column = 31 } },
+
+        .{ .type = .@"const", .lexeme = "const", .location = .{ .line = 2, .column = 1 } },
+        .{ .type = .identifier, .lexeme = "num", .location = .{ .line = 2, .column = 7 } },
+        .{ .type = .@":", .lexeme = ":", .location = .{ .line = 2, .column = 10 } },
+        .{ .type = .float, .lexeme = "float", .location = .{ .line = 2, .column = 12 } },
+        .{ .type = .@"=", .lexeme = "=", .location = .{ .line = 2, .column = 18 } },
+        .{ .type = .number, .lexeme = "12.34", .location = .{ .line = 2, .column = 20 } },
+        .{ .type = .@";", .lexeme = ";", .location = .{ .line = 2, .column = 25 } },
+
+        .{ .type = .EOF, .lexeme = "", .location = .{ .line = 3, .column = 1 } },
     };
 
-    try testing.expectEqualSlices(Token, expected_tokens, tokens);
+    try expectTokensEqual(tokens, expected);
 }
