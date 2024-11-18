a13b342d1b62196ac085a8e19b1be71ec93c377f
Niklas Koll <nkoll@databay.de>
2024-11-18 07:59:28 +0100

    Greatly improved main function

--- src/main.zig
+++ src/main.zig
@@ -2,9 +2,7 @@ const std = @import("std");
 const Token = @import("Token.zig");
 
 pub fn main() !void {
-    for (Token.ReservedKeywords.keys(), Token.ReservedKeywords.values()) |key, value| {
-        std.debug.print("Reserved Keyword: '{s}' maps to {}\n", .{ key, value });
-    }
+    std.debug.print("🚧Under Construction🚧\n", .{});
 }
 
 test "Token" {
