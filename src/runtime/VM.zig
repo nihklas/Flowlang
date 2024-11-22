0da36061d81c10b5c95e74e7927bd9eb7f41ea93
Niklas Koll <niklas@nkoll.de>
2024-11-22 20:53:01 +0100

    Added TODO and stubs for run functions

--- src/runtime/VM.zig
+++ src/runtime/VM.zig
@@ -7,7 +7,20 @@ pub fn init(alloc: Allocator) VM {
     };
 }
 
+// TODO: Write two main run functions:
+//    1. while(true) with switch
+//    2. labeled switch continue
+// Compare those in benchmarks
 pub fn run(self: *VM) !void {
+    self.runWhileSwitch();
+    // self.runSwitchContinue();
+}
+
+fn runWhileSwitch(self: *VM) !void {
+    _ = self;
+}
+
+fn runSwitchContinue(self: *VM) !void {
     _ = self;
 }
 
