07f5fce6c91042975cfa5b4c349c6b1d97d27b2f
Niklas Koll <niklas@nkoll.de>
2024-12-07 21:17:55 +0100

    Added imports for unit testing other modules as well

--- src/testing.zig
+++ src/testing.zig
@@ -1,8 +1,24 @@
 test "Compiler" {
-    _ = @import("compiler/main.zig");
-    _ = @import("compiler/Token.zig");
-    _ = @import("compiler/Scanner.zig");
-    _ = @import("compiler/ast.zig");
-    _ = @import("compiler/Parser.zig");
     _ = @import("compiler/Compiler.zig");
+    _ = @import("compiler/Parser.zig");
+    _ = @import("compiler/Scanner.zig");
+    _ = @import("compiler/Sema.zig");
+    _ = @import("compiler/Token.zig");
+    _ = @import("compiler/ast.zig");
+    _ = @import("compiler/main.zig");
+}
+
+test "Runtime" {
+    _ = @import("runtime/GC.zig");
+    _ = @import("runtime/VM.zig");
+    _ = @import("runtime/main.zig");
+}
+
+test "Shared" {
+    _ = @import("shared/debug/BytecodeDumper.zig");
+    _ = @import("shared/builtins.zig");
+    _ = @import("shared/byte_code.zig");
+    _ = @import("shared/definitions.zig");
+    _ = @import("shared/root.zig");
+    _ = @import("shared/stack.zig");
 }
