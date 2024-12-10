f9bb93946a07f6b0bcde8572835b4c93b06a49d4
Niklas Koll <niklas@nkoll.de>
2024-12-10 18:51:07 +0100

    Cleaned ByteCodeDumper

--- src/shared/debug/BytecodeDumper.zig
+++ src/shared/debug/BytecodeDumper.zig
@@ -1,6 +1,7 @@
 ip: usize,
 code: []const u8,
-constants: [256]Value = undefined,
+constants: [256]FlowValue = undefined,
+constant_counter: usize = 0,
 
 pub fn dump(code: []const u8) void {
     var dumper: Dumper = .{
@@ -11,80 +12,15 @@ pub fn dump(code: []const u8) void {
 }
 
 fn runDump(self: *Dumper) void {
-    var constant_counter: usize = 0;
-    var constant_block: bool = true;
-    var functions_block: bool = false;
     while (self.ip < self.code.len) {
         defer std.debug.print("\n", .{});
         std.debug.print("{x:0>4} ", .{self.ip});
         const op = self.instruction();
         switch (op) {
-            .integer => {
-                defer constant_counter += 1;
-                defer self.ip += 8;
-                const bytes = self.code[self.ip .. self.ip + 8];
-                const int = std.mem.bytesToValue(Integer, bytes);
-                self.constants[constant_counter] = .{ .int = int };
-                printInstruction("OP_INTEGER", "{d: <10}{d}", .{ constant_counter, int });
-            },
-            .float => {
-                defer constant_counter += 1;
-                defer self.ip += 8;
-                const bytes = self.code[self.ip .. self.ip + 8];
-                const float = std.mem.bytesToValue(Float, bytes);
-                self.constants[constant_counter] = .{ .float = float };
-                printInstruction("OP_FLOAT", "{d: <10}{d}", .{ constant_counter, float });
-            },
-            .string => {
-                defer constant_counter += 1;
-                const len = self.code[self.ip];
-                self.ip += 1;
-                self.constants[constant_counter] = .{ .string = self.code[self.ip .. self.ip + len] };
-                printInstruction("OP_STRING", "{d: <10}{s}", .{
-                    constant_counter,
-                    self.constants[constant_counter].string,
-                });
-                self.ip += len;
-            },
-            .string_long => {
-                defer constant_counter += 1;
-                const bytes = self.code[self.ip .. self.ip + 4];
-                self.ip += 4;
-                const len = std.mem.bytesToValue(u32, bytes);
-                self.constants[constant_counter] = .{ .string = self.code[self.ip .. self.ip + len] };
-                printInstruction("OP_STRING_LONG", "{d: <10}{s}", .{
-                    constant_counter,
-                    self.constants[constant_counter].string,
-                });
-                self.ip += len;
-            },
             .constant => {
                 const idx = self.byte();
                 const value = self.constants[idx];
                 printInstruction("OP_CONSTANT", "{d: <10}{}", .{ idx, value });
-            },
-            .jump_if_false => {
-                defer self.ip += 2;
-                const jump_len = std.mem.bytesToValue(u16, self.code[self.ip .. self.ip + 2]);
-                printInstruction("OP_JUMP_IF_FALSE", "-> {x:0>4}", .{jump_len + self.ip + 2});
-            },
-            .jump => {
-                defer self.ip += 2;
-                const jump_len = std.mem.bytesToValue(u16, self.code[self.ip .. self.ip + 2]);
-                printInstruction("OP_JUMP", "-> {x:0>4}", .{jump_len + self.ip + 2});
-            },
-            .jump_back => {
-                defer self.ip += 2;
-                const jump_len = std.mem.bytesToValue(u16, self.code[self.ip .. self.ip + 2]);
-                printInstruction("OP_JUMP_BACK", "-> {x:0>4}", .{self.ip - jump_len + 2});
-            },
-            .get_local => {
-                const local_idx = self.byte();
-                printInstruction("OP_GET_LOCAL", "{d}", .{local_idx});
-            },
-            .set_local => {
-                const local_idx = self.byte();
-                printInstruction("OP_SET_LOCAL", "{d}", .{local_idx});
             },
             .function => {
                 defer self.ip += 2;
@@ -94,6 +30,15 @@ fn runDump(self: *Dumper) void {
                 const line_count = std.mem.bytesToValue(u16, self.code[self.ip .. self.ip + 2]);
                 printInstruction("OP_FUNCTION", "{d: <10}{{{s}}} [{x:0>4}]", .{ arg_count, name, self.ip + line_count });
             },
+            .integer => self.constantInstruction("OP_INTEGER", .int),
+            .float => self.constantInstruction("OP_FLOAT", .float),
+            .string => self.constantInstruction("OP_STRING", .string),
+            .string_long => self.constantInstruction("OP_STRING_LONG", .string_long),
+            .get_local => self.localInstruction("OP_GET_LOCAL"),
+            .set_local => self.localInstruction("OP_SET_LOCAL"),
+            .jump_if_false => self.jumpInstruction("OP_JUMP_IF_FALSE", true),
+            .jump => self.jumpInstruction("OP_JUMP", true),
+            .jump_back => self.jumpInstruction("OP_JUMP_BACK", false),
             .call => printInstruction("OP_CALL", "", .{}),
             .create_global => printInstruction("OP_CREATE_GLOBAL", "", .{}),
             .get_global => printInstruction("OP_GET_GLOBAL", "", .{}),
@@ -117,15 +62,8 @@ fn runDump(self: *Dumper) void {
             .equal => printInstruction("OP_EQUAL", "", .{}),
             .unequal => printInstruction("OP_UNEQUAL", "", .{}),
             .@"return" => printInstruction("OP_RETURN", "", .{}),
-            .constants_done => {
-                constant_block = false;
-                functions_block = true;
-                printInstruction("OP_CONSTANTS_DONE", "", .{});
-            },
-            .functions_done => {
-                functions_block = false;
-                printInstruction("OP_FUNCTIONS_DONE", "", .{});
-            },
+            .constants_done => printInstruction("OP_CONSTANTS_DONE", "", .{}),
+            .functions_done => printInstruction("OP_FUNCTIONS_DONE", "", .{}),
         }
     }
 }
@@ -133,6 +71,47 @@ fn runDump(self: *Dumper) void {
 fn printInstruction(op: []const u8, comptime fmt: []const u8, args: anytype) void {
     std.debug.print("{s: <24} ", .{op});
     std.debug.print(fmt, args);
+}
+
+fn jumpInstruction(self: *Dumper, op: []const u8, forward: bool) void {
+    defer self.ip += 2;
+    const jump_len = std.mem.bytesToValue(u16, self.code[self.ip .. self.ip + 2]);
+    const target = if (forward) self.ip + jump_len + 2 else self.ip - jump_len + 2;
+    printInstruction(op, "-> {x:0>4}", .{target});
+}
+
+fn localInstruction(self: *Dumper, op: []const u8) void {
+    const operand = self.byte();
+    printInstruction(op, "{d}", .{operand});
+}
+
+fn constantInstruction(self: *Dumper, op: []const u8, flow_type: enum { int, float, string, string_long }) void {
+    defer self.constant_counter += 1;
+    switch (flow_type) {
+        .int, .float => {
+            defer self.ip += 8;
+            const bytes = self.code[self.ip .. self.ip + 8];
+            if (flow_type == .int) {
+                self.constants[self.constant_counter] = .{ .int = std.mem.bytesToValue(Integer, bytes) };
+            } else {
+                self.constants[self.constant_counter] = .{ .float = std.mem.bytesToValue(Float, bytes) };
+            }
+        },
+        .string => {
+            const len = self.code[self.ip];
+            self.ip += 1;
+            self.constants[self.constant_counter] = .{ .string = self.code[self.ip .. self.ip + len] };
+            self.ip += len;
+        },
+        .string_long => {
+            const bytes = self.code[self.ip .. self.ip + 4];
+            self.ip += 4;
+            const len = std.mem.bytesToValue(u32, bytes);
+            self.constants[self.constant_counter] = .{ .string = self.code[self.ip .. self.ip + len] };
+            self.ip += len;
+        },
+    }
+    printInstruction(op, "{d: <10}{}", .{ self.constant_counter, self.constants[self.constant_counter] });
 }
 
 fn instruction(self: *Dumper) OpCode {
@@ -152,4 +131,4 @@ const std = @import("std");
 const Integer = @import("../definitions.zig").Integer;
 const Float = @import("../definitions.zig").Float;
 const OpCode = @import("../byte_code.zig").OpCode;
-const Value = @import("../definitions.zig").FlowValue;
+const FlowValue = @import("../definitions.zig").FlowValue;
