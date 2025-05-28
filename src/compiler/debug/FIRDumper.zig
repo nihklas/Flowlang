050c1f5545f783e8f0dc393395ab1d8dea1397c9
Niklas Koll <nkoll@databay.de>
2025-05-28 09:29:58 +0200

    Started with FIRDumping, use arena for internal arrays

--- src/compiler/debug/FIRDumper.zig
+++ src/compiler/debug/FIRDumper.zig
@@ -1,6 +1,29 @@
 pub fn dump(writer: anytype, fir: *const FIR) !void {
-    _ = writer;
-    _ = fir;
+    if (fir.nodes.items.len == 0) return;
+
+    for (fir.constants.items, 0..) |constant, idx| {
+        try writer.print("Constant: C{d} = {}\n", .{ idx, constant });
+    }
+    try writer.writeAll("\n");
+
+    for (fir.nodes.items) |node| {
+        switch (node.kind) {
+            .expr => {
+                try dumpExpr(writer, fir, node.index);
+            },
+            .cond => @panic("'cond' is not yet supported in FIRDumper"),
+        }
+        try writer.writeAll(";\n");
+    }
+}
+
+fn dumpExpr(writer: anytype, fir: *const FIR, expr_idx: usize) !void {
+    const expr = fir.exprs.items[expr_idx];
+    switch (expr.op) {
+        .literal => try writer.print("C{d}", .{expr.operands[0]}),
+        else => std.debug.panic("'{s}' is not yet supported in FIRDumper", .{@tagName(expr.op)}),
+    }
 }
 
 const FIR = @import("../ir/FIR.zig");
+const std = @import("std");
--- src/compiler/ir/FIR.zig
+++ src/compiler/ir/FIR.zig
@@ -15,6 +15,8 @@
 // - list of constants
 // - entrypoint
 alloc: Allocator,
+arena_state: std.heap.ArenaAllocator,
+arena: Allocator,
 constants: std.ArrayListUnmanaged(FlowValue) = .empty,
 nodes: std.ArrayListUnmanaged(Node) = .empty,
 exprs: std.ArrayListUnmanaged(Node.Expr) = .empty,
@@ -23,8 +25,12 @@ conds: std.ArrayListUnmanaged(Node.Cond) = .empty,
 // TODO: how do they look?
 // functions: std.ArrayListUnmanaged(),
 
+/// Generally, all Nodes are Statements. That means, no Node produces a value.
+/// The actual type of Statement is encoded in the `kind` field. Depending on the value there, the
+/// `index` field points into a different collection. `before` and `after` contain the index of the
+/// previous and next node in the list of instructions.
 const Node = struct {
-    kind: enum { expr, cond, pop },
+    kind: enum { expr, cond },
     index: usize,
     // TODO: should this be a slice?
     before: ?usize = null,
@@ -36,7 +42,7 @@ const Node = struct {
     };
 
     const Cond = struct {
-        /// must point to a node of kind `expr`
+        /// points to a node of kind `expr`
         condition: usize,
         true: []usize,
         false: []usize,
@@ -44,10 +50,17 @@ const Node = struct {
 };
 
 pub fn init(alloc: Allocator) FIR {
-    return .{ .alloc = alloc };
+    var fir: FIR = .{
+        .alloc = alloc,
+        .arena_state = .init(alloc),
+        .arena = undefined,
+    };
+    fir.arena = fir.arena_state.allocator();
+    return fir;
 }
 
 pub fn deinit(self: *FIR) void {
+    self.arena_state.deinit();
     self.constants.deinit(self.alloc);
     self.nodes.deinit(self.alloc);
     self.exprs.deinit(self.alloc);
@@ -79,20 +92,25 @@ fn traverseStmt(self: *FIR, stmt: *ast.Stmt) void {
 fn traverseExpr(self: *FIR, expr: *const ast.Expr) usize {
     switch (expr.*) {
         .literal => {
-            const flowvalue: FlowValue = switch (expr.literal.value) {
-                .int => |int| .{ .int = int },
-                .float => |float| .{ .float = float },
-                .bool => |boolean| .{ .bool = boolean },
-                .null => .null,
-                .string => |string| .{ .string = string },
-                .function, .builtin_fn => unreachable,
-            };
-            const constant_idx = self.resolveConstant(flowvalue);
-            self.exprs.append(self.alloc, .{ .op = .literal, .operands = &.{constant_idx} }) catch oom();
+            const operands = self.arena.alloc(usize, 1) catch oom();
+            operands[0] = self.resolveConstant(resolveFlowValue(expr));
+            self.exprs.append(self.alloc, .{ .op = .literal, .operands = operands }) catch oom();
             return self.exprs.items.len - 1;
         },
         else => std.debug.panic("Expression '{s}' is not yet supported", .{@tagName(expr.*)}),
     }
+}
+
+fn resolveFlowValue(expr: *const ast.Expr) FlowValue {
+    std.debug.assert(expr.* == .literal);
+    return switch (expr.literal.value) {
+        .int => |int| .{ .int = int },
+        .float => |float| .{ .float = float },
+        .bool => |boolean| .{ .bool = boolean },
+        .null => .null,
+        .string => |string| .{ .string = string },
+        .function, .builtin_fn => unreachable,
+    };
 }
 
 fn resolveConstant(self: *FIR, value: FlowValue) usize {
