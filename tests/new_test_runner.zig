b546d83ca1cd367de39f6fd7212e51e255abcb26
Niklas Koll <niklas@nkoll.de>
2025-07-03 21:10:59 +0200

    Use case flag as a filter, use new integration tests in pipeline

--- tests/new_test_runner.zig
+++ tests/new_test_runner.zig
@@ -9,6 +9,11 @@ comptime {
     if (SPLIT_MARKER_STDERR.len != SPLIT_MARKER_LEN) @compileError("Split Marker for STDERR has a wrong length");
 }
 
+const OutputPipe = enum {
+    stdout,
+    stderr,
+};
+
 const Result = enum {
     success,
     failure,
@@ -39,104 +44,88 @@ pub fn main() u8 {
         return 1;
     };
 
-    if (args.next()) |case| {
-        const case_path = std.fmt.allocPrint(gpa, "{s}/{s}.flow", .{ cases_dir, case }) catch return 1;
-        defer gpa.free(case_path);
+    const file_filter = args.next() orelse "";
 
-        const case_file = std.fs.openFileAbsolute(case_path, .{}) catch |err| switch (err) {
-            error.FileNotFound => {
-                printStdErr("Test Case not found: {s}\n", .{case_path});
-                return 1;
-            },
-            else => {
-                printStdErr("Error on file opening: {}\n", .{err});
-                return 1;
-            },
-        };
-        defer case_file.close();
+    var state: SharedState = .{
+        .mutex = .{},
+        .alloc = gpa,
+        .results = .empty,
+    };
+    defer state.results.deinit(gpa);
 
-        var test_case = buildTestCase(gpa, case_file, case) catch return 1;
-        defer test_case.free(gpa);
+    // Pipeline should look like this:
+    // - Iterate over all test cases
+    // - Hand work for each file directly over to thread pool
+    // - for each test case:
+    //      - open and read the file
+    //      - look for the split marks and split source code and assertions
+    //      - run source code
+    //      - perform assertions
+    //      - acquire results_mutex and save result in the shared result state
+    //      - on failure, aquire print_mutex and print out the failed assertions
+    // - Print out statistics
 
-        runTestCase(gpa, compiler, test_case) catch return 1;
-    } else {
-        var state: SharedState = .{
-            .mutex = .{},
-            .alloc = gpa,
-            .results = .empty,
-        };
-        defer state.results.deinit(gpa);
+    {
+        var test_dir = std.fs.openDirAbsolute(cases_dir, .{ .iterate = true }) catch return 1;
+        defer test_dir.close();
 
-        // Pipeline should look like this:
-        // - Iterate over all test cases
-        // - Hand work for each file directly over to thread pool
-        // - for each test case:
-        //      - open and read the file
-        //      - look for the split marks and split source code and assertions
-        //      - run source code
-        //      - perform assertions
-        //      - acquire results_mutex and save result in the shared result state
-        //      - on failure, aquire print_mutex and print out the failed assertions
-        // - Print out statistics
+        var pool: std.Thread.Pool = undefined;
+        pool.init(.{
+            .allocator = gpa,
+        }) catch return 1;
+        defer pool.deinit();
 
-        {
-            var test_dir = std.fs.openDirAbsolute(cases_dir, .{ .iterate = true }) catch return 1;
-            defer test_dir.close();
+        var dir_iter = test_dir.iterate();
+        while (dir_iter.next() catch return 1) |entry| {
+            if (entry.kind != .file) continue;
 
-            var pool: std.Thread.Pool = undefined;
-            pool.init(.{
-                .allocator = gpa,
-            }) catch return 1;
-            defer pool.deinit();
-
-            var dir_iter = test_dir.iterate();
-            while (dir_iter.next() catch return 1) |entry| {
-                if (entry.kind != .file) continue;
-
-                pool.spawn(workerFn, .{ test_dir, entry.name, compiler, &state }) catch return 1;
+            if (std.mem.indexOf(u8, entry.name, file_filter) == null) {
+                continue;
             }
+
+            pool.spawn(workerFn, .{ test_dir, entry.name, compiler, &state }) catch return 1;
         }
+    }
 
-        printStdOut("\n\n------------------------------\n\n", .{});
+    printStdOut("\n------------------------------\n\n", .{});
 
-        var stats: std.AutoHashMapUnmanaged(Result, u16) = .empty;
-        defer stats.deinit(gpa);
-        stats.ensureTotalCapacity(gpa, 3) catch return 1;
+    var stats: std.AutoHashMapUnmanaged(Result, u16) = .empty;
+    defer stats.deinit(gpa);
+    stats.ensureTotalCapacity(gpa, 3) catch return 1;
 
-        stats.putAssumeCapacity(.success, 0);
-        stats.putAssumeCapacity(.failure, 0);
-        stats.putAssumeCapacity(.crash, 0);
+    stats.putAssumeCapacity(.success, 0);
+    stats.putAssumeCapacity(.failure, 0);
+    stats.putAssumeCapacity(.crash, 0);
 
-        var iter = state.results.iterator();
-        while (iter.next()) |entry| {
-            const count = stats.getPtr(entry.value_ptr.*).?;
-            count.* += 1;
+    var iter = state.results.iterator();
+    while (iter.next()) |entry| {
+        const count = stats.getPtr(entry.value_ptr.*).?;
+        count.* += 1;
 
-            const icon = switch (entry.value_ptr.*) {
-                .success => "✅",
-                .failure => "❌",
-                .crash => "💀",
-            };
+        const icon = switch (entry.value_ptr.*) {
+            .success => "✅",
+            .failure => "❌",
+            .crash => "💀",
+        };
 
-            printStdOut("{s} Test Case: {s}\n", .{ icon, entry.key_ptr.* });
+        printStdOut("{s} Test Case: {s}\n", .{ icon, entry.key_ptr.* });
 
-            // stdout.print("{s} ({s}){s} Test Case: {s}\n", .{
-            //     icon,
-            //     @tagName(entry.value_ptr.*),
-            //     if (entry.value_ptr.* == .crash) "  " else "",
-            //     entry.key_ptr.*,
-            // }) catch return 1;
-        }
+        // stdout.print("{s} ({s}){s} Test Case: {s}\n", .{
+        //     icon,
+        //     @tagName(entry.value_ptr.*),
+        //     if (entry.value_ptr.* == .crash) "  " else "",
+        //     entry.key_ptr.*,
+        // }) catch return 1;
+    }
 
-        printStdOut("\n-----------------------------------\n{d} Tests Succeeded, {d} Tests Failed", .{ stats.get(.success).?, stats.get(.failure).? });
-        if (stats.get(.crash).? > 0) {
-            printStdOut(", {d} Tests crashed", .{stats.get(.crash).?});
-        }
-        printStdOut("\n", .{});
+    printStdOut("\n-----------------------------------\n{d} Tests Succeeded, {d} Tests Failed", .{ stats.get(.success).?, stats.get(.failure).? });
+    if (stats.get(.crash).? > 0) {
+        printStdOut(", {d} Tests crashed", .{stats.get(.crash).?});
+    }
+    printStdOut("\n", .{});
 
-        if (stats.get(.failure).? > 0 or stats.get(.crash).? > 0) {
-            return 1;
-        }
+    if (stats.get(.failure).? > 0 or stats.get(.crash).? > 0) {
+        return 1;
     }
 
     return 0;
@@ -240,105 +229,6 @@ fn doTest(dir: std.fs.Dir, sub_path: []const u8, compiler: []const u8, state: *S
         defer state.mutex.unlock();
 
         state.results.put(state.alloc, sub_path, .success) catch return;
-    }
-}
-
-const OutputPipe = enum {
-    stdout,
-    stderr,
-};
-const TestCase = struct {
-    tmp_file_path: []const u8,
-    name: []const u8,
-    assertion: []const u8,
-    output: OutputPipe,
-
-    fn free(self: *TestCase, gpa: std.mem.Allocator) void {
-        gpa.free(self.name);
-        gpa.free(self.assertion);
-        gpa.free(self.tmp_file_path);
-    }
-};
-
-fn buildTestCase(gpa: std.mem.Allocator, case: std.fs.File, case_name: []const u8) !TestCase {
-    const content = try case.readToEndAlloc(gpa, MAX_TEST_CASE_SIZE);
-    defer gpa.free(content);
-
-    const maybe_stdout = std.mem.indexOf(u8, content, SPLIT_MARKER_STDOUT);
-    const maybe_stderr = std.mem.indexOf(u8, content, SPLIT_MARKER_STDERR);
-
-    if (maybe_stdout != null and maybe_stderr != null) {
-        printStdErr("In test case {s} there are both stdout and stderr assertions. Only one at a time can be applied.\n", .{case_name});
-        return error.WrongAssertions;
-    }
-
-    const assertions_start, const output: OutputPipe = blk: {
-        if (maybe_stdout) |stdout| {
-            break :blk .{ stdout, .stdout };
-        }
-        if (maybe_stderr) |stderr| {
-            break :blk .{ stderr, .stderr };
-        }
-
-        printStdErr("In test case {s} are no assertions.\n", .{case_name});
-        return error.MissingAssertions;
-    };
-
-    const source = std.mem.trim(u8, content[0..assertions_start], " \n");
-    const assertion = content[assertions_start + SPLIT_MARKER_LEN ..];
-
-    const tmp_file_path = try std.fmt.allocPrint(gpa, "/tmp/{s}.flow", .{case_name});
-
-    {
-        const source_file = try std.fs.createFileAbsolute(tmp_file_path, .{});
-        defer source_file.close();
-        try source_file.writeAll(source);
-    }
-
-    return .{
-        .name = try gpa.dupe(u8, case_name),
-        .assertion = try gpa.dupe(u8, assertion),
-        .output = output,
-        .tmp_file_path = tmp_file_path,
-    };
-}
-
-fn runTestCase(gpa: std.mem.Allocator, compiler: []const u8, test_case: TestCase) !void {
-    var child = std.process.Child.init(&.{ compiler, "--run", test_case.tmp_file_path }, gpa);
-    child.stderr_behavior = .Pipe;
-    child.stdout_behavior = .Pipe;
-
-    var stdout: std.ArrayListUnmanaged(u8) = .empty;
-    defer stdout.deinit(gpa);
-
-    var stderr: std.ArrayListUnmanaged(u8) = .empty;
-    defer stderr.deinit(gpa);
-
-    try child.spawn();
-    try child.collectOutput(gpa, &stdout, &stderr, MAX_OUTPUT_BYTES);
-    const term = try child.wait();
-
-    if (term != .Exited) {
-        printStdErr("Test Case got interrupted\n", .{});
-        return error.ExecutionInterrupted;
-    }
-
-    if (term.Exited != 0) {
-        if (test_case.output != .stderr) {
-            printStdErr("Expected the test program to succeed, but it failed. TestCase: {s}\n", .{test_case.name});
-            printStdErr("Error Output:\n{s}\n", .{stderr.items});
-            printStdErr("======================\n", .{});
-            return error.TermMismatch;
-        }
-        try expectEqualStrings(test_case.assertion, stderr.items);
-    } else {
-        if (test_case.output != .stdout) {
-            printStdErr("Expected the test program to fail, but it succeeded. TestCase: {s}\n", .{test_case.name});
-            printStdErr("Succesfull Output:\n{s}\n", .{stdout.items});
-            printStdErr("======================\n", .{});
-            return error.TermMismatch;
-        }
-        try expectEqualStrings(test_case.assertion, stdout.items);
     }
 }
 
